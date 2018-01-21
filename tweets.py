3b2d9916050d226b11648d453cd91a630cdfb2c4
Shen Gong <shngong@gmail.com>
2018-01-20 19:33:57 -0800

    add HTTP request methods to TweetHelper

--- tweets.py
+++ tweets.py
@@ -1,6 +1,8 @@
 import oauth2 as oauth
 import time
 
+from urllib.parse import urlencode
+
 class TweetHelper():
     """
     Helper class for accessing Twitter APIs. Takes care
@@ -10,6 +12,7 @@ class TweetHelper():
     def __init__(self, consumer, token):
         self.consumer = consumer
         self.token = token
+        self.client = oauth.Client(consumer, token)
 
     def _get_oauth_params(self):
         return {
@@ -20,12 +23,21 @@ class TweetHelper():
             'oauth_token': self.token.key,
         }
 
-    def _generate_request(self, url, method, post_params=None):
+    def _generate_request(self, url, method, additional_params=None):
         params = self._get_oauth_params()
-        if post_params:
-            params.update(post_params)
+        if additional_params:
+            params.update(additional_params)
 
         req = oauth.Request(method=method, url=url, parameters=params)
         req.sign_request(oauth.SignatureMethod_HMAC_SHA1(), self.consumer, self.token)
 
         return req
+
+    def get(self, url, params=None):
+        if params:
+            url = url + '?' + urlencode(params)
+
+        return self.client.request(url, headers=self._get_oauth_params())
+
+    def post(self, url, params=None):
+        self.client.request(url, method='POST', headers=self._get_oauth_params(), body=urlencode(params))
