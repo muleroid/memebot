f3001527bddcf02887988d8e94593501d4d09a0f
Shen Gong <sgong@lyft.com>
2018-12-26 14:23:59 -0600

    integrate tokenizer into markov chain class

--- text_generate/markov_chain.py
+++ text_generate/markov_chain.py
@@ -1,4 +1,5 @@
 from collections import defaultdict
+from text_generate.tokenizer import Tokenizer
 import random
 
 class ProbabilityDistribution():
@@ -10,36 +11,40 @@ class ProbabilityDistribution():
 		self._distribution = defaultdict(int)
 		self._total = 0
 
-	def add_token(self, token):
-		self._distribution[token] += 1
+	def add_word(self, word):
+		self._distribution[word] += 1
 		self._total += 1
 
 	def get(self):
 		index = random.random() * self._total
 		running = 0
 
-		for token, count in self._distribution.items():
+		for word, count in self._distribution.items():
 			running += count
 			if running >= index:
-				return token
+				return word
 
 class MarkovChain():
 	"""
 	Can this even be called a markov chain?
 	"""
-	def __init__(self):
+	def __init__(self, n=1):
 		self.token_map = defaultdict(ProbabilityDistribution)
+		self.order = n
 
-	def prep_chain(self, tokens):
-		for i in range(len(tokens) - 1):
-			token = tokens[i]
-			distribution = self.token_map[token]
-			distribution.add_token(tokens[i+1])
+	def parse_string(self, s):
+		tokenizer = Tokenizer(self.order, s)
+		results = tokenizer.generate_tokens()
+		for tokens, word in results:
+			distribution = self.token_map[tokens]
+			distribution.add_word(word)
 
-	def get_next_token(self, token):
+	def get_next_word(self, tokens):
 		"""
-		Given a token, returns the next token given
+		Given tokens, returns the next word given
 		the weighted distribution of the current state
 		"""
-		distribution = self.token_map[token]
+		if len(tokens) != self.order:
+			raise ValueError('Input tokens must be same order as Markov Chain')
+		distribution = self.token_map[tokens]
 		return distribution.get()
--- text_generate/tokenizer.py
+++ text_generate/tokenizer.py
@@ -2,24 +2,26 @@ class Tokenizer():
 	"""
 	Simple n-order tokenizer.
 	"""
-	def __init__(self, n):
+	def __init__(self, n, s):
+		"""
+		Initializes tokenizer. Given a string input,
+		splits it into tokens.
+		"""
 		self.order = n
+		self.words = [word.strip() for word in s.split()]
 		self.results = []
-
-	def generate_tokens(self, s):
-		"""
-		Given a string input, breaks it into tokens
-		and generates a list mapping n tokens to the
-		next token.
-		"""
-		tokens = [token.strip() for token in s.split()]
-		if len(tokens) <= self.order:
+		if len(self.words) <= self.order:
 			raise ValueError('Tokenized input is shorter than order of tokenizer')
-		for i in range(self.order, len(tokens)):
-			n_tokens = tuple(tokens[i - j] for j in range(self.order, 0, -1))
-			self.results.append((n_tokens, tokens[i]))
 
-		# also log the ending token
-		end_tokens = tuple(tokens[i] for i in range(self.order * -1, 0))
-		self.results.append((end_tokens, None))
+	def generate_tokens(self):
+		"""
+		Generates tokens based on words.
+		"""
+		for i in range(self.order, len(self.words)):
+			n_words = tuple(self.words[i - j] for j in range(self.order, 0, -1))
+			self.results.append((n_words, self.words[i]))
+
+		# also log the ending words
+		end_words = tuple(self.words[i] for i in range(self.order * -1, 0))
+		self.results.append((end_words, None))
 		return self.results
