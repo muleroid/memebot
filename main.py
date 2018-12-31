b66b8319e4149577efa218044c4e3835530b7dc5
Shen Gong <sgong@lyft.com>
2018-12-31 12:01:55 -0600

    fix everything

--- main.py
+++ main.py
@@ -1,4 +1,4 @@
-from bottle import Bottle, get, request
+from bottle import Bottle, get, request, run
 from typing import List
 import json
 
@@ -10,10 +10,14 @@ SECRET_SAUCE = ['dril', 'sosadtoday', 'PakaluPapitio']
 app = Bottle()
 
 
-@get('/tweets/<username>')
+@app.get('/tweets/<username>')
 def get_tweets(username):
     start_time_ms = int(request.query.start_time_ms)
     end_time_ms = int(request.query.end_time_ms)
 
     tweets = tweet_scraper.scrape_tweets(username, start_time_ms, end_time_ms)
     return json.dumps({'tweets': tweets})
+
+
+if __name__ == '__main__':
+    run(app, host='localhost', port=8080)
