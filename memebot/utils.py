87d273670c47d1dccfa34d1d5f5a9887000b7fb3
Shen Gong <sgong@lyft.com>
2018-12-31 17:15:17 -0600

    thinking outside the box

--- memebot/utils.py
+++ memebot/utils.py
@@ -7,13 +7,12 @@ METADATA_HEADERS = {
 }
 
 
-def get_project_id():
+def get_project_id() -> bool:
     r = requests.get('http://metadata.google.internal/computeMetadata/v1/project/project-id', headers=METADATA_HEADERS)
     if r.status_code != 200:
         raise RuntimeError(f'Got an error requesting metadata [{r.status_code}]: {r.text}')
     return r.text
 
 
-def is_running_on_app_engine():
-    env = os.environ.get('NODE_ENV', '')
-    return env == 'production'
+def is_running_on_app_engine() -> bool:
+    return os.environ.get('IS_GAE')
